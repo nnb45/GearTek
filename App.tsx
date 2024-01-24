4bee8b1d777777ff4e8f77d2810f7a2f25efe74b
loc01227175513 <51218059+loc01227175513@users.noreply.github.com>
2024-01-24 23:06:48 +0700

    feadtures
    feadtures

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import { Image, StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
-import ProfileScreen from './src/screens/Profile/ProfileScreen'
-import ProductCart from './src/screens/Home/ProductCart'
-import SanPham from './src/screens/Home/sanpham'
-import LoginScreen from './src/screens/User/LoginScreen'
-import Cart from './src/screens/Home/Cart'
-import { color } from './src/themes/theme'
+import { Image, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import ProfileScreen from './src/screens/Profile/ProfileScreen';
+import ProductCart from './src/screens/Home/ProductCart';
+import SanPham from './src/screens/Home/sanpham';
+import LoginScreen from './src/screens/User/LoginScreen';
+import Cart from './src/screens/Home/Cart';
+import Features from './src/screens/Home/Feadtures.tsx'; // Corrected import
 
 const App = () => {
-    return (
-       <SafeAreaView style={{flex: 1}}>
-                <Cart/>
-              {/* <ProductCart/> */}
-              {/* <LoginScreen/> */}
-       </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Features />
+      {/* <Cart/> */}
+      {/* <ProductCart/> */}
+      {/* <LoginScreen/> */}
+    </SafeAreaView>
+  );
+};
 
-export default App
-
+export default App;
